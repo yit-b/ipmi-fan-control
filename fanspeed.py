a541a84ec417ffc5c864025753578cccbd5b71f1
wyatt <wyatt@yitb.io>
2023-05-31 19:55:33 -0400

    Use the max of all CPU core temps, some light refactoring

--- fanspeed.py
+++ fanspeed.py
@@ -3,6 +3,7 @@ import time
 import json
 import sched
 import math
+from typing import List
 
 UPPER_CPU_TEMP = 80.0
 LOWER_CPU_TEMP = 50.0
@@ -13,7 +14,7 @@ LOWER_GPU_TEMP = 50.0
 FULL_FAN_SPEED = 100
 IDLE_FAN_SPEED = 30
 
-def curve(norm_temp):
+def fan_curve(norm_temp):
     # Generalised logistic function
     # https://en.wikipedia.org/wiki/Generalised_logistic_function
     # Slowly ramp fan speed as lower temperature is crossed but quickly increase after
@@ -29,70 +30,53 @@ def curve(norm_temp):
     fan_speed = A + ((K-A) / (C+Q * math.exp(-B * norm_temp + F) ) ** (1 / v)) + G
     return fan_speed
 
-
-def get_cpu_temp(normalized=True):
-    cpu_temp_check_cmd = "ipmitool sdr type temperature"
-    p = subprocess.run(cpu_temp_check_cmd.split(" "), check=True, capture_output=True)
-    table = p.stdout.decode("utf-8").splitlines()
-
-    rows = {}
-    for row in table:
-        r = [e.strip() for e in row.split("|")]
-        name = r[0]
-        temp = r[-1]
-        if temp == "No Reading":
-            temp = "0"
-        rows[name] = int(temp.split(" degrees C")[0])
-
-    cpu_temp = rows["CPU Temp"]
-    print(f"CPU Temp: {cpu_temp}C")
-    if not normalized:
-        return cpu_temp
-
-    norm_temp = min((max(cpu_temp, LOWER_CPU_TEMP) - LOWER_CPU_TEMP) / (UPPER_CPU_TEMP - LOWER_CPU_TEMP), 1.0)
-    return norm_temp
-
 def norm(val, min_val, max_val):
     return min((max(val, min_val) - min_val) / (max_val - min_val), 1.0)
 
-def get_cpu_temp_json():
+def denorm(val, min_val, max_val):
+    return val * (max_val - min_val) + min_val
+
+def get_cpu_temps() -> List[float]:
     cpu_temp_check_cmd = "sensors -j"
     p = subprocess.run(cpu_temp_check_cmd.split(" "), check=True, capture_output=True)
     temps_json = json.loads(p.stdout.decode("utf-8"))
-    cpu_temp = int(temps_json["coretemp-isa-0000"]["Package id 0"]["temp1_input"])
-    cpu_norm = norm(cpu_temp, LOWER_CPU_TEMP, UPPER_CPU_TEMP)
-    return (cpu_temp, cpu_norm)
+    cpu_temps = temps_json["coretemp-isa-0000"]
+    core_keys = [k for k in cpu_temps if k.startswith("Core ")]
+    core_temp_objects = [cpu_temps[k] for k in core_keys]
+    temps = []
+    for o in core_temp_objects:
+        temps.append([v for k,v in o.items() if k.endswith("_input")][0])
+    return temps
 
-def get_gpu_temp():
+def get_gpu_temps() -> List[float]:
     gpu_temp_check_cmd = "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader"
     p = subprocess.run(gpu_temp_check_cmd.split(" "), check=True, capture_output=True)
-
-    temps = [int(e) for e in p.stdout.decode("utf-8").split("\n") if e]
-    max_gpu_temp = max(temps)
-    gpu_norm = norm(max_gpu_temp, LOWER_GPU_TEMP, UPPER_GPU_TEMP)
-
-    return (temps, gpu_norm)
+    temps = list(map(int, p.stdout.decode("utf-8").strip().split("\n")))
+    return temps
 
 def set_fans(scheduler):
     scheduler.enter(1, 1, set_fans, (scheduler,))
 
-    cpu_temp, cpu_norm = get_cpu_temp_json()
-    gpu_temp, gpu_norm = get_gpu_temp()
+    cpu_temps = get_cpu_temps()
+    cpu_temps_normalized = [norm(t, LOWER_CPU_TEMP, UPPER_CPU_TEMP) for t in cpu_temps]
 
-    max_norm = max(cpu_norm, gpu_norm)
-    norm_fan = curve(max_norm)
-    denorm_fan = int(norm_fan * (FULL_FAN_SPEED - IDLE_FAN_SPEED) + IDLE_FAN_SPEED)
+    gpu_temps = get_gpu_temps()
+    gpu_temps_normalized = [norm(t, LOWER_GPU_TEMP, UPPER_GPU_TEMP) for t in gpu_temps]
+
+    norm_fan = fan_curve(max(gpu_temps_normalized + cpu_temps_normalized))
+    denorm_fan = int(denorm(norm_fan, IDLE_FAN_SPEED, FULL_FAN_SPEED))
 
     for group_idx in range(0, 2):
         fan_speed_set_cmd = f"ipmitool raw 0x30 0x70 0x66 0x01 {hex(group_idx)} {hex(denorm_fan)}"
         p = subprocess.run(fan_speed_set_cmd.split(" "), check=True, capture_output=True)
-    print(f"CPU: {cpu_temp}C, GPU: {gpu_temp}C, Fan speed: {denorm_fan}%")
+        
+    print(f"CPU(s): {cpu_temps}C, GPU(s): {gpu_temps}C, Fan speed: {denorm_fan}%")
 
 def main():
 
     # import matplotlib.pyplot as plt
     # import numpy as np
-    # plt.plot([i for i in np.arange(0, 1.2, 0.05)], [curve(i) * (FULL_FAN_SPEED - IDLE_FAN_SPEED) + IDLE_FAN_SPEED for i in np.arange(0, 1.2, 0.05)])
+    # plt.plot([i for i in np.arange(0, 1.2, 0.05)], [fan_curve(i) * (FULL_FAN_SPEED - IDLE_FAN_SPEED) + IDLE_FAN_SPEED for i in np.arange(0, 1.2, 0.05)])
     # plt.ylabel("Fan speed %")
     # plt.xlabel("Normalized Temperature")
     # plt.show()
@@ -100,7 +84,7 @@ def main():
 
     # for i in np.arange(20, 100, 5):
     #     norm_temp = norm(i, LOWER_CPU_TEMP, UPPER_CPU_TEMP)
-    #     norm_fan = curve(norm_temp)
+    #     norm_fan = fan_curve(norm_temp)
     #     denorm_fan = int(norm_fan * (FULL_FAN_SPEED - IDLE_FAN_SPEED) + IDLE_FAN_SPEED)
     #     print(f"Temp: {i}, fans: {denorm_fan}")
     # return
